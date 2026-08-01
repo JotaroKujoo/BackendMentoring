1083d6da77b2bb1371a8d8780a57372a2577a944
JotaroKujoo <rdrgz.jose123@gmail.com>
2026-08-01 21:26:11 +0200

    add: Todas las excepciones y capturas de error necesarias añadidas, correccion de bugs

--- exercises/kata1.py
+++ exercises/kata1.py
@@ -1,73 +1,67 @@
-isOn = True
+is_on = True
 
-while isOn:
-    word = input("Introduce una palabra: ")
     
-    def displayWord(word):
-        print("Texto original: ",word)
+def analyze_word(word):
+    print("Texto original: ", word)
+    print("Primer caracter: ", word[0])
+    print("Ultimo caracter: ", word[-1])
+    print("Primeros 4 caracteres: ", word[:4])
+    print("ultimos 4 caracteres: ", word[-4:])
     
-    def displayFirstLetter(word):
-        print("Primer caracter: ",word[0])
+    inverted= ""
+    for letter in word:
+        inverted = letter + inverted
+    
+    print("Texto invertido: ", inverted)
+    
+    print("Numero de caracteres: ", len(word))
+    print("Caracteres en posiciones pares")
+    
+    for index, caracter in enumerate(word):
+        if index % 2 == 0:
+            print(caracter)
+    print("Caracteres en posiciones impares")
+    
+    for index, caracter in enumerate(word):
+        if index % 2 != 0:
+            print(caracter)
 
-    def displayLastLetter(word):
-        print("Ultimo caracter: ",word[-1])
-
-    def displayFirst4Letters(word):
-        print("Primeros 4 caracteres: ", word[:4])
-    
-    def displayLast4Letters(word):
-        print("ultimos 4 caracteres: ", word[-4:])
-        
-    def displayInvertedWord(word):
-        inverted= ""
-        for letter in word:
-            inverted = letter + inverted
-        
-        print("Texto invertido: ", inverted)
-    
-    def displayWordLenght(word):
-        print("Numero de caracteres: ", len(word))
-        
-    def displayEvenIndex(word):
-        print("Caracteres en posiciones pares")
-        for index, caracter in enumerate(word):
-            if index%2==0:
-                print(caracter)
-    
-    def displayNonEvenIndex(word):
-        print("Caracteres en posiciones impares")
-        for index, caracter in enumerate(word):
-            if index%2!=0:
-                print(caracter)
-        
-    def analyze_word(word):
-        displayWord(word)
-        displayFirstLetter(word)
-        displayLastLetter(word)
-        displayFirst4Letters(word)
-        displayLast4Letters(word)
-        displayInvertedWord(word)
-        displayWordLenght(word)
-        displayEvenIndex(word)    
-        displayNonEvenIndex(word)
-                
-
-
-    if len(word) > 0 & word.isalpha():
-        analyze_word(word)
+def choice_selector():
+    intentos = 0
+    max_intentos = 3
+    while intentos < max_intentos:
         print("Que deseas hacer? \n 1. Reintentar \n 2. Salir")
-        option = int(input("Introduce la opcion: "))
+        print(f"\nTienes {(max_intentos - intentos)} intentos")
+        try:
+            option = int(input("\nIntroduce la opcion: "))
+            
+        except ValueError:
+            print("\nDebes introducir un numero")
+            intentos += 1    
+            continue
+          
         if option == 1:
-            print("Reintentando...")
-        if option == 2:
+            print("\nReintentando...")
+            return True
+        
+        elif option == 2:
             print("Cerrando el programa")
-            isOn = False
+            return False
+    
+        else:
+            intentos += 1
+            print("\nOpcion no valida")
+            
+    
+        
+    print("Demasiados intentos, cerrando..")
+    return False
+
+while is_on:
+    word = input("Introduce una palabra: ")
+    if word.isalpha():
+        analyze_word(word)
+        is_on = choice_selector()
     else:
         print("La palabra debe contener al menos 1 caracter para ser evaluada y no debe contener numeros")
-        print("Que deseas hacer? \n 1. Reintentar \n 2. Salir")
-        option = int(input("Introduce la opcion: "))
-        if option == 1:
-            print("Reintentando...")
-        if option == 2:
-            print("Cerrando el programa")
-            isOn = False
+        is_on = choice_selector()
